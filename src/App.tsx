2bb18d6c69122f5cff57403a4b9de555b02873e5
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2022-04-22 00:13:50 -0300

    source control

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from "@mui/material";
 import { BrowserRouter, } from "react-router-dom";
 import { AppRoutes } from "./routes/index"
+import { LigthTheme } from './themes/ligth'
 export const App = () => {
   return (
+   <ThemeProvider theme={LigthTheme}>
    <BrowserRouter>
     <AppRoutes/>
    </BrowserRouter>
+   </ThemeProvider> 
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
    
      <Routes>
-       <Route path="/pag-inicial" element={<p>pagina inicial</p>}/>
+       <Route path="/pag-inicial" element={<Button variant="contained" color="primary">teste</Button>}/>
        <Route path="*" element={<Navigate to="/pag-inicial"/>}/>
 
 
